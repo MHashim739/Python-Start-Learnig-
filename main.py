62f20d73c12de30fe2f370c7ccc99ef57e8ae308
Muhammad hashim <mh4165209@gmail.com>
2026-06-13 13:25:48 -0700

    Day 01 In Python Learning

---
+++ main.py
@@ -0,0 +1,38 @@
+                                    #Start Learning In Python Documentation Geek For Geek
+                                    # First topic cover that document Geek For Geek Input & Output 
+# print("hello World !")
+# name=input("Enter Your Name");
+# print("hello " , name , " ! Welcome");
+# name="Muhammed";
+# print("hello " , name);
+# name2="Anjila";
+# age=23;
+# education="university";
+# print("Student Name " , ":" , name2 , ";"  , "Student Age " , ":" , age , ";" , "Student Coolification " , ":" , education , ";");
+# name=" Khatija" ;
+# name02=" Shanawar" ;
+# print(name .split());
+# print(name02 .split());
+
+# userInput=input("Enter Any Two Number");
+# print(userInput.split());
+# decimalNo=12.002;
+# intNo=4;
+# print(int(decimalNo));
+# print(intNo);
+# name=input();
+# print(name);
+# name="Hashim";
+# id=12;
+# print(name.split());
+# print(id);
+                                            #this variable name are valid in python
+# name="has".split();
+# name_01="Kaif".split();
+# __name="wahaz".split();
+# print(name , name_01 , __name)
+
+                                            # this variable are invalid in python
+#1name="has";
+#int= this variable name are use in decimal number to convert to integer number;
+#name-01= "Kasif";
