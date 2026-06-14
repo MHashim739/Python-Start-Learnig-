7d3919b16e4776ea724dff48173d8d37aa3e5371
Muhammad hashim <mh4165209@gmail.com>
2026-06-14 10:26:02 -0700

    day 02 python learning

--- main.py
+++ main.py
@@ -36,3 +36,115 @@
 #1name="has";
 #int= this variable name are use in decimal number to convert to integer number;
 #name-01= "Kasif";
+# x=10;
+# y=14;
+# z="Hello World !";
+# x="Hi ";
+# z=y;
+# print(x , y , z);
+# x = y = z = 100 ,"Hello ! " .split() , "Worlds !" .split();
+# print(x , y , z);
+# name  , id , UserNo = "Ahsan" , 20202 , 20;
+# workSplit=name.split();
+# saveIdAndUserNo="User Id" , id , "User Number ",  UserNo;
+# print(name , workSplit , saveIdAndUserNo);
+# name="Muhammed";
+# name_02=name;
+# name_02="Ahsan";
+# id=20202;
+# id_02=id;
+# id=id_02+1;
+# print(name);
+# print(name_02);
+# print(id);
+# print(id_02);
+# name="Muhammed";
+# name_02="Hashim";
+# print("User Name Length" ,len(name));
+# print("User Name_02 Length" , len(name_02));
+                                                         #Python Operators
+
+# a , b=10, 20;
+# print("Addition", a + b);
+# print("Subtraction", a - b);
+# print("Multiply" , a *b);
+# print("Division",a / b);
+# print("floor Division", a // b);
+# print("modulus", a % b);
+# print("Exponenttiation" , a**b);
+# a=13;
+# b=33;
+# print(a > b);
+# print(a < b);
+# print(a == b);
+# print(a != b);
+# print(a >= b);
+# print(a <= b);
+# a=True;
+# b=False;
+# print(a and b);
+# print(a or b);
+# print(not b);
+# name=input("Enter Your Name");
+# id=input("Enter Your Id");
+# print(name==name and id==id);
+# userAge=input("Enter Your Age");
+# userName=input("enter Your name");
+# print(userAge  < 18 ,  "do'nt ask vote" or userAge >= 18  , "You ask vote me" , userName);
+# print(12 & 3);
+# print(5 & 3);
+# print(12 | 5);
+# print(5 ^ 3);
+# print(5 ^ 5);
+# print(6 ^ 6);
+# print(5 | 1);
+# print(1 | 0);
+# print(~4);
+# print(~10)
+# print(5 << 1);
+# print(5 >>3)
+# print(5 >>2)
+# print(5 >>4)
+# print(5,3/2);
+# print(5,2/2);
+# print(5 , 4/2);
+# a=10;
+# b=a;
+# print(b);
+# a+=b;
+# print(a);
+# b-=a;
+# print(b);
+# a*=b;
+# print(a);
+# b <<= a;
+# print(b);
+a = 10
+b = a
+# print(b)
+# b += a
+# print(b)
+# b -= a
+# print(b)
+# b *= a
+# print(b)
+# b <<= a
+# print(b)
+# a=10;
+# b=30;
+# c=a;
+# print(a is b);
+# print(a is not b);
+# print(a is c);
+# print(c is a);
+age_01=20;
+age_02=55;
+my_list=[20,22,30 ,33,40,44];
+if(age_02 not in my_list):
+    print(age_02,"this Number Not Present In My List",my_list);
+else:
+    print("this Number present in Given List");
+if(age_01 in my_list):
+    print(age_01,"This Number Is Present In This List",my_list);
+else:
+    print("this Number Not Present in This list");
